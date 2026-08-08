9cf39c4d042bf25279242655054c2a298feb9360
psychofine <rs174mg@mail.ru>
2026-08-08 20:37:49 +0300

    Delete Data Structures & Algorithms/two-integer-sum directory

--- Data Structures & Algorithms/two-integer-sum/submission-0.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def twoSum(self, n: List[int], t: int) -> List[int]:
-        for i in range(len(n)):
-            for j in range(len(n)):
-                if i!=j and n[i]+n[j]==t:
-                    return [i,j]
-
-        
