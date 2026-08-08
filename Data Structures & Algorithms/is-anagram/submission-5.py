e0dd34ceaca9e0234261b141ea413cec85810232
psychofine <rs174mg@mail.ru>
2026-08-08 20:37:41 +0300

    Delete Data Structures & Algorithms/is-anagram directory

--- Data Structures & Algorithms/is-anagram/submission-5.py
+++
@@ -1,8 +0,0 @@
-from string import printable
-class Solution:
-    def isAnagram(self, s: str, t: str) -> bool:
-        if set(s)==set(t) and len(s)==len(t):
-            if all(s.count(i)==t.count(i) for i in set(s)):
-                return True
-        return False
-        
